81fcf489c965c68b6418ec5fbedd7c35848ffdb1
DESKTOP-ABVEVU9\atsio <atsiorvas@innovative.gr>
2021-12-26 17:01:51 +0200

    adding unit tests

---
+++ src/tests/UnitTests/ApplicationCore/Entities/CustomerTests/BasketAddItem.cs
@@ -0,0 +1,14 @@
+﻿using BlazorApp.ApplicationCore.Entities;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.eShopWeb.UnitTests.ApplicationCore.Entities.CustomerTests {
+
+    public class CustomerAddItem {
+
+        [Fact]
+        public void AddsBasketItemIfNotPresent() {
+            var customer = new Customer();
+        }
+    }
+}
---
+++ src/tests/UnitTests/ApplicationCore/Services/CustomerServiceTests/AddItemToBasket.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using BlazorApp.ApplicationCore.Entities;
+using BlazorApp.ApplicationCore.Interfaces;
+using Moq;
+using Xunit;
+
+namespace Microsoft.eShopWeb.UnitTests.ApplicationCore.Services.CustomerServiceTests {
+
+    public class AddItemToBasket {
+        private readonly Mock<IRepository<Customer>> _mockCustomerRepo = new Mock<IRepository<Customer>>();
+
+        [Fact]
+        public async Task InvokesBasketRepositoryGetBySpecAsyncOnce() {
+            var customer = new Customer();
+        }
+    }
+}
